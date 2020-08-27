c2bd29ba1e33284c76d27bd4f7309a69c4e44863
Jacky <chikonwong@gmail.com>
2020-08-27 15:01:27 +0800

    Update Sidebar.vue

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,6 +1,19 @@
 <template>
     <div id="app">
-        <p>{{message}}</p>
+      <template>
+  <div>
+    <b-button v-b-toggle.sidebar-1>Menu</b-button>
+    <b-sidebar id="sidebar-1" title="Meun" shadow>
+      <div class="px-3 py-2">
+        <p>
+          Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis
+          in, egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
+        </p>
+       
+      </div>
+    </b-sidebar>
+  </div>
+</template>
     </div>
 </template>
 
@@ -14,5 +27,7 @@ export default {
 }
 </script>
 <style>
-
+  b-button{
+    align-content: center;
+  }
 </style>
