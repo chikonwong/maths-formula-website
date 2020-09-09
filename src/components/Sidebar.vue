8b22ac16b47496a548031d0059696e33f1e2d17d
Jacky <chikonwong@gmail.com>
2020-09-09 22:15:57 +0800

    Update Sidebar.vue

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,14 +1,20 @@
 <template>
   <div>
-    MENU<ul class="list-unstyled right-align" >
-      <li><router-link to="/">Home</router-link></li>
-    <li><router-link to="/EstimationAndError">Estimation and Error Formula</router-link></li>
-  <li><router-link to="/Percentage">Percentage</router-link></li>
-  <!-- <li><router-link to="/Estimation_and_Error_Formula">Estimation_and_Error_Formula</router-link></li>
-  <li><router-link to="/Estimation_and_Error_Formula">Estimation_and_Error_Formula</router-link></li>
-  <li><router-link to="/Estimation_and_Error_Formula">Estimation_and_Error_Formula</router-link></li>
-  <li><router-link to="/Estimation_and_Error_Formula">Estimation_and_Error_Formula</router-link></li> -->
-  </ul>
+    MENU<ul class="list-unstyled right-align">
+      <li>
+        <router-link to="/">Home</router-link>
+      </li>
+      <li>
+        <router-link to="/EstimationAndError">Estimation and Error Formula</router-link>
+      </li>
+      <li>
+        <router-link to="/Percentage">Percentage</router-link>
+      </li>
+      <li>
+      </li>
+      
+
+    </ul>
   </div>
 
 </template>
