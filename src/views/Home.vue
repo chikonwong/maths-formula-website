a101c26625ff40db8c3fe58ff42d94d24000e155
Jacky <chikonwong@gmail.com>
2020-10-05 20:41:38 +0800

    update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="col-8">
        <div v-for="item in info" :key="item.sub_topic">
-      {{ item.sub_topic }}
+      {{ item.sub_topic }}this
       {{ item.formula }}
     </div>
     </div>
